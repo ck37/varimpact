255b3392b363300c5e6a4ffca96b5661a957bc76
Chris Kennedy <chrisken@gmail.com>
2016-06-06 21:19:04 -0700

    Use 2000 samples in test

--- tests/testthat/testVarImpact.R
+++ tests/testthat/testVarImpact.R
@@ -5,7 +5,7 @@ library(testthat)
 context("Dataset A - continuous only variables")
 
 set.seed(1)
-N = 200
+N = 2000
 
 num_normal = 10
 X = as.data.frame(matrix(rnorm(N * num_normal), N, num_normal))
@@ -15,9 +15,6 @@ Y = rbinom(N, 1, plogis(.2*X[, 1] + .1*X[, 2] - .2*X[, 3] + .1*X[, 3]*X[, 4] - .
 # This works.
 vim = varImpact(Y = Y, data = X, V = 2, verbose=T)
 vim
-names(vim)
-
 vim$results_all
-vim$results_consistent
-
+names(vim)
 exportLatex(vim, dir = "results")
