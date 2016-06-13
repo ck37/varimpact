af88947914193c0221d89cbedec80d062f4530c2
Chris Kennedy <chrisken@gmail.com>
2016-06-13 13:27:07 -0700

    Add tests for factors-only, factors-and-numeric, and parallelization for all 3 types

--- tests/testthat/testVarImpact.R
+++ tests/testthat/testVarImpact.R
@@ -16,7 +16,7 @@ Y = rbinom(N, 1, plogis(.2*X[, 1] + .1*X[, 2] - .2*X[, 3] + .1*X[, 3]*X[, 4] - .
 miss_num = 10
 for (i in 1:5) X[sample(nrow(X), 1), sample(ncol(X), 1)] = NA
 
-# This works.
+# Basic test.
 vim = varImpact(Y = Y, data = X, V = 2, verbose=T)
 vim
 vim$results_all
@@ -25,8 +25,46 @@ exportLatex(vim, dir = "results")
 
 
 # Test imputation
-vim = varImpact(Y = Y, data = X, V = 2, verbose=T, impute="median")
-vim = varImpact(Y = Y, data = X, V = 2, verbose=T, impute="knn")
+vim = varImpact(Y = Y, data = X, verbose=T, impute="median")
+vim = varImpact(Y = Y, data = X, verbose=T, impute="knn")
 
-#context("Dataset B: factor variables")
-#context("Dataset C: numeric and factor variables")
+
+# Test parallelization.
+doMC::registerDoMC()
+# Check how many cores we're using.
+foreach::getDoParWorkers()
+vim = varImpact(Y = Y, data = X, verbose=T, impute="knn")
+
+# Test disabling parallelization.
+vim = varImpact(Y = Y, data = X, verbose=T, impute="knn", parallel = F)
+# Return to single core usage.
+foreach::registerDoSEQ()
+foreach::getDoParWorkers()
+
+context("Dataset B: factor variables")
+
+X_fac = data.frame(lapply(1:ncol(X), FUN=function(col_i) as.factor(floor(abs(pmin(pmax(X[, col_i], -1), 1)*3)))))
+dim(X_fac)
+colnames(X_fac) = paste0("fac_", 1:ncol(X_fac))
+colnames(X_fac)
+summary(X_fac)
+
+# Basic factor test.
+vim = varImpact(Y = Y, data = X_fac[, 1:5], V = 2, verbose=T)
+
+# Test parallelization.
+doMC::registerDoMC()
+# Check how many cores we're using.
+foreach::getDoParWorkers()
+
+# Factor variables with parallelization.
+vim = varImpact(Y = Y, data = X_fac, verbose=T, impute="knn")
+vim
+
+context("Dataset C: numeric and factor variables")
+
+X_combined = cbind(X[1:4], X_fac[5:7])
+
+# Basic factor test.
+vim = varImpact(Y = Y, data = X_combined, V = 2, verbose=T)
+vim
