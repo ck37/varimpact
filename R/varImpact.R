f4850f1e9ffc2a08e9735fd909f22636d2735f41
Chris Kennedy <chrisken@gmail.com>
2016-06-14 09:17:52 -0700

    Clean up print.varImpact based on cran checks. Add importFrom for stats functions in varImpact.

--- R/varImpact.R
+++ R/varImpact.R
@@ -59,6 +59,8 @@
 #'
 #' @return Results object.
 #'
+#' @importFrom stats cor model.matrix na.omit pnorm quantile var
+#'
 #' @examples
 #' # Create test dataset.
 #' set.seed(1)
@@ -1246,19 +1248,21 @@ exportLatex = function(impact_results, outname = "", dir = ".") {
 #' Shows the consistent results by default. If there are no consistent results
 #' it shows all results.
 #'
-#' @param obj Results object from varImpact.
+#' @param x Results object from varImpact.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-print.varImpact = function(obj) {
+print.varImpact = function(x, ...) {
   # Just print the consistent results.
-  if (nrow(obj$results_consistent) > 0) {
+  if (nrow(x$results_consistent) > 0) {
     cat("Consistent results:\n")
-    print(obj$results_consistent)
+    print(x$results_consistent)
   } else {
     cat("No consistent results.\n")
     cat("All results:\n")
-    print(obj$results_all)
+    print(x$results_all)
   }
+  invisible(x)
 }
 
 
