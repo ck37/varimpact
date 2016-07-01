2f6047782ddda3576e1e30c0a73ed2bea45ffab9
Chris Kennedy <chrisken@gmail.com>
2016-06-30 17:41:23 -0700

    Add tests for Y = gaussian rather than binary

--- tests/testthat/testVarImpact.R
+++ tests/testthat/testVarImpact.R
@@ -10,34 +10,38 @@ N = 200
 num_normal = 7
 X = as.data.frame(matrix(rnorm(N * num_normal), N, num_normal))
 
-Y = rbinom(N, 1, plogis(.2*X[, 1] + .1*X[, 2] - .2*X[, 3] + .1*X[, 3]*X[, 4] - .2*abs(X[, 4])))
+# Binary distribution via the binomial.
+Y_bin = rbinom(N, 1, plogis(.2*X[, 1] + .1*X[, 2] - .2*X[, 3] + .1*X[, 3]*X[, 4] - .2*abs(X[, 4])))
+# Gaussian distribution.
+Y_gaus = .2*X[, 1] + .1*X[, 2] - .2*X[, 3] + .1*X[, 3]*X[, 4] - .2*abs(X[, 4]) + rnorm(N, 0, 1)
 
 # Add some missing data to X.
 miss_num = 10
 for (i in 1:miss_num) X[sample(nrow(X), 1), sample(ncol(X), 1)] = NA
 
-# Basic test.
-vim = varImpact(Y = Y, data = X[, 1:4], V = 2, verbose=T)
+# Basic test - binary outcome.
+vim = varImpact(Y = Y_bin, data = X[, 1:4], V = 2, verbose=T)
 vim
 vim$results_all
 # names(vim)
 exportLatex(vim, dir = "results")
-
+# And a gaussian outcome.
+vim = varImpact(Y = Y_gaus, data = X[, 1:4], V = 2, verbose=T, family="gaussian")
 
 # Test imputation
-vim = varImpact(Y = Y, data = X[, 1:4], verbose=T, impute="zero")
-vim = varImpact(Y = Y, data = X[, 1:4], verbose=T, impute="median")
-vim = varImpact(Y = Y, data = X[, 1:4], verbose=T, impute="knn")
+vim = varImpact(Y = Y_bin, data = X[, 1:4], verbose=T, impute="zero")
+vim = varImpact(Y = Y_bin, data = X[, 1:4], verbose=T, impute="median")
+vim = varImpact(Y = Y_bin, data = X[, 1:4], verbose=T, impute="knn")
 
 
 # Test parallelization via doMC.
 doMC::registerDoMC()
 # Check how many cores we're using.
 foreach::getDoParWorkers()
-vim = varImpact(Y = Y, data = X[, 1:4], verbose=T)
+vim = varImpact(Y = Y_bin, data = X[, 1:4], verbose=T)
 
 # Test disabling parallelization.
-vim = varImpact(Y = Y, data = X[, 1:4], verbose=T, parallel = F)
+vim = varImpact(Y = Y_bin, data = X[, 1:4], verbose=T, parallel = F)
 # Return to single core usage.
 foreach::registerDoSEQ()
 foreach::getDoParWorkers()
@@ -48,7 +52,7 @@ doSNOW::registerDoSNOW(cluster)
 # Check that we're using the snow cluster.
 foreach::getDoParName()
 foreach::getDoParWorkers()
-vim = varImpact(Y = Y, data = X[, 1:4], verbose=T)
+vim = varImpact(Y = Y_bin, data = X[, 1:4], verbose=T)
 vim
 snow::stopCluster(cluster)
 # Return to single core usage.
@@ -63,7 +67,9 @@ colnames(X_fac)
 summary(X_fac)
 
 # Basic factor test.
-vim = varImpact(Y = Y, data = X_fac[, 1:4], V = 2, verbose=T)
+vim = varImpact(Y = Y_bin, data = X_fac[, 1:4], V = 2, verbose=T)
+# And gaussian
+vim = varImpact(Y = Y_gaus, data = X_fac[, 1:4], V = 2, verbose=T, family="gaussian")
 
 # Test parallelization.
 doMC::registerDoMC()
@@ -71,7 +77,7 @@ doMC::registerDoMC()
 foreach::getDoParWorkers()
 
 # Factor variables with parallelization.
-vim = varImpact(Y = Y, data = X_fac[, 1:4], verbose=T)
+vim = varImpact(Y = Y_bin, data = X_fac[, 1:4], verbose=T)
 vim
 
 context("Dataset C: numeric and factor variables")
@@ -81,8 +87,10 @@ context("Dataset C: numeric and factor variables")
 X_combined = cbind(X[1:3], X_fac[5:7])
 
 # Basic combined test.
-vim = varImpact(Y = Y, data = X_combined, V = 2, verbose=T)
+vim = varImpact(Y = Y_bin, data = X_combined, V = 2, verbose=T)
 vim
+# And gaussian
+vim = varImpact(Y = Y_gaus, data = X_combined, V = 2, verbose=T, family="gaussian")
 
 #################################
 # mlbench BreastCancer dataset.
@@ -97,3 +105,4 @@ table(data$Y)
 doMC::registerDoMC()
 # This takes 1-3 minutes.
 vim = varImpact(Y = data$Y, data = subset(data, select=-c(Y, Class, Id)))
+vim
