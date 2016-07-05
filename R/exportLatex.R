5ecd37d0d3a48368ad18e75292f1a4fe4d1a82d0
Chris Kennedy <chrisken@gmail.com>
2016-07-05 15:40:26 -0700

    Support digits option for xtable

--- R/exportLatex.R
+++ R/exportLatex.R
@@ -14,28 +14,29 @@
 #' @param impact_results Result object from previous varImpact() call.
 #' @param outname (Optional) String that is prepended to filenames.
 #' @param dir (Optional) Directory to save the results, defaults to current directory.
+#' @param digits Digits to round numbers, passed through to xtable.
 #'
 #' @seealso
 #' \code{\link[varImpact]{varImpact}}
 #'
 #' @export
-exportLatex = function(impact_results, outname = "", dir = ".") {
+exportLatex = function(impact_results, outname = "", dir = ".", digits = 4) {
   print(xtable::xtable(impact_results$results_by_fold,
                        caption = "Variable Importance Results By Estimation Sample",
-                       label = "byV", digits = 4),
+                       label = "byV", digits = digits),
         type = "latex", file = paste0(paste(dir, outname, sep="/"), "varimpByV.tex"),
         caption.placement = "top", include.rownames = T)
 
   print(xtable::xtable(impact_results$results_all,
                        caption = "Variable Importance Results for Combined Estimates",
-                       label = "allRes", digits = 4),
+                       label = "allRes", digits = digits),
         type = "latex", file = paste0(paste(dir, outname, sep="/"), "varimpAll.tex"),
         caption.placement = "top", include.rownames = T)
 
 
   print(xtable::xtable(impact_results$results_consistent,
                        caption = "Subset of of Significant and ``Consistent'' Results",
-                       label = "consisRes", digits = 4),
+                       label = "consisRes", digits = digits),
         type = "latex", file = paste0(paste(dir, outname, sep="/"), "varimpConsistent.tex"),
         caption.placement = "top", include.rownames = T)
 }
