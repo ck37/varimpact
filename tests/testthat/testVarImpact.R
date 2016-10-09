7504483b5458f2173900c562ae856f2c047c1d34
Chris Kennedy <chrisken@gmail.com>
2016-10-09 15:34:48 -0700

    Add test for extra code coverage (all p-values significant)

--- tests/testthat/testVarImpact.R
+++ tests/testthat/testVarImpact.R
@@ -152,3 +152,25 @@ if (.Platform$GUI == "RStudio") {
 vim = varImpact(Y = data$Y, X)
 vim$time
 vim
+
+context("Dataset D: basic example")
+
+####################################
+# Create test dataset.
+set.seed(1)
+N <- 100
+num_normal <- 7
+X <- as.data.frame(matrix(rnorm(N * num_normal), N, num_normal))
+Y <- rbinom(N, 1, plogis(.2*X[, 1] + .1*X[, 2] - .2*X[, 3] + .1*X[, 3]*X[, 4] - .2*abs(X[, 4])))
+# Add some missing data to X so we can test imputation.
+for (i in 1:10) X[sample(nrow(X), 1), sample(ncol(X), 1)] <- NA
+
+####################################
+# Basic example
+vim <- varImpact(Y = Y, data = X)
+vim
+vim$results_all
+# In this test all variables are significant, which is rare.
+exportLatex(vim)
+# Clean up
+file.remove(c("varimpByV.tex", "varImpAll.tex", "varimpConsistent.tex"))
