a740e6ab1227645b9becdc3b48d757d7116fcde8
Chris Kennedy <chrisken@gmail.com>
2016-12-28 11:56:45 -0600

    Clarify that default print output is significant and consistent

--- R/print.varImpact.R
+++ R/print.varImpact.R
@@ -1,19 +1,19 @@
 #' Custom printing of the varImpact results.
 #'
-#' Shows the consistent results by default. If there are no consistent results
-#' it shows all results.
+#' Shows the significant and consistent results by default. If there are no
+#' consistent results it shows all results.
 #'
 #' @param x Results object from varImpact.
 #' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
 print.varImpact = function(x, ...) {
-  # Just print the consistent results.
+  # Just print the significant and consistent results.
   if (nrow(x$results_consistent) > 0) {
-    cat("Consistent results:\n")
+    cat("Significant and consistent results:\n")
     print(x$results_consistent)
   } else {
-    cat("No consistent results.\n")
+    cat("No significant and consistent results.\n")
     cat("All results:\n")
     print(x$results_all)
   }
