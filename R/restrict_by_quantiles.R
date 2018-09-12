5b9b91dbc8f266fa99c2e4ee8c58214d04463913
Chris Kennedy <chrisken@gmail.com>
2018-09-12 08:25:04 -0700

    restrict_by_quantiles: add verbose argument, not yet used

--- R/restrict_by_quantiles.R
+++ R/restrict_by_quantiles.R
@@ -2,21 +2,26 @@
 #'
 #' @param data Dataframe or matrix
 #' @param quantile_probs The probabilities corresponding to the quantiles that will be compared.
+#' @param verbose If TRUE output additional details during execution.
 #'
 #' @return New dataframe with the restriction applied.
 #'
 #' @seealso quantiles_equivalent
 #'
 #' @export
-restrict_by_quantiles = function(data, quantile_probs = c(0.1, 0.9)) {
+restrict_by_quantiles =
+  function(data,
+           quantile_probs = c(0.1, 0.9),
+           verbose = FALSE) {
 
   # Drop column if the two quantiles have the same sample value (i.e. difference = 0).
   # True = remove, False = keep
   # TODO: support parallelization, e.g. for very wide datasets.
-  drop_cols = sapply(1:ncol(data), FUN=function(i) quantiles_equivalent(data[, i], quantile_probs))
+  drop_cols = sapply(1:ncol(data),
+                     function(i) quantiles_equivalent(data[, i], quantile_probs))
 
   # Restrict to variables with sufficient variation.
-  data = data[, !drop_cols, drop = F]
+  data = data[, !drop_cols, drop = FALSE]
 
   return(data)
 }
