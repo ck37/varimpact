5fb00f8f4df36a6abc571f4f0d60e8f6364615f9
Chris Kennedy <chrisken@gmail.com>
2018-09-12 08:53:14 -0700

    separate_factors_numerics: add verbose argument, not yet used

--- R/separate_factors_numerics.R
+++ R/separate_factors_numerics.R
@@ -3,13 +3,16 @@
 #' @param data A dataframe or matrix.
 #' @param strings_to_factors Convert character strings to factors if True,
 #'   otherwise ignore character string variables.
+#' @param verbose If TRUE output additional details during execution.
 #'
 #' @return Results list.
 #'
 #' Convert strings to factors as well.
 #'
 #' @export
-separate_factors_numerics = function(data, strings_to_factors = T) {
+separate_factors_numerics =
+  function(data, strings_to_factors = TRUE,
+           verbose = FALSE) {
 
   if (strings_to_factors) {
     # Identify character strings.
@@ -21,11 +24,11 @@ separate_factors_numerics = function(data, strings_to_factors = T) {
 
   # Identify factors.
   is_factor = sapply(data, is.factor)
-  df_factors = data[, is_factor, drop=F]
+  df_factors = data[, is_factor, drop = FALSE]
 
   # Identify numerics.
   is_numeric = sapply(data, is.numeric)
-  df_numerics = data[, is_numeric, drop=F]
+  df_numerics = data[, is_numeric, drop = FALSE]
 
   result = list(df_factors = df_factors, df_numerics = df_numerics)
   return(result)
